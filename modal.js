ce82ab5562c9cdf05691e91334f664ba0d20179c
Icarus <astrikort24@gmail.com>
2020-12-11 16:18:37 +0200

    fixed button issue and added form interactivity

--- modal.js
+++ modal.js
@@ -1,21 +1,25 @@
-const plusButton = document.getElementById('plus');
-const myModalElement = document.getElementById('my-modal');
-plusButton.addEventListener('click', () => {
-    myModalElement.style.display = 'block';
+const plusButton = document.getElementById("plus");
+const myModalElement = document.getElementById("my-modal");
+
+plusButton.addEventListener("click", () => {
+  myModalElement.style.display = "block";
 });
 
-const closeModalButton = document.querySelector('div.close');
-closeModalButton.addEventListener('click',()=>{
-    myModalElement.style.display = 'none';
-    resetModalInput();
-})
+const closeModalButton = document.querySelector("div.close");
+closeModalButton.addEventListener("click", () => {
+  myModalElement.style.display = "none";
+  resetModalInput();
+});
 
-function resetModalInput(){
-    const inputTextElements = document.querySelectorAll('input[type="text"]');
-    const inputNumberElement = document.querySelector('input[type="number"]');
-    Array.from(inputTextElements).forEach(inputTextElement =>{
-        inputTextElement.value = '';
-    })
-    inputNumberElement.value = '';
+function resetModalInput() {
+  const radioButtons = document.querySelectorAll('input[name="read?"]');
+  const inputTextElements = document.querySelectorAll('input[type="text"]');
+  const inputNumberElement = document.querySelector('input[type="number"]');
+  [...inputTextElements].forEach((inputTextElement) => {
+    inputTextElement.value = "";
+  });
+  [...radioButtons].forEach((radioButton) => {
+    radioButton.checked = false;
+  });
+  inputNumberElement.value = "";
 }
-
